f5cc4aaf86035e086d635f0c8fca7f6a73806402
Shawn Zhong <public@shawnzhong.com>
2019-05-06 13:28:26 -0500

    [7.2] fix fuzz using address sanitizer

--- 7.2_Dynamic_Tools/fuzz/report/countchar.c
+++ 7.2_Dynamic_Tools/fuzz/report/countchar.c
@@ -8,12 +8,12 @@ void initcharcount ()
     cnttable = (int *)malloc(sizeof(int) * 100000);
 }
 
-void countchar (char c)
+void countchar (unsigned char c) // c used to be signed. If c > 127 => c is negative
 {
     int i;
 
-    i = (c*10000+7) % 100000;
-    cnttable[i]++;
+    i = (c*10000+7) % 100000; // c is used to ca
+    cnttable[i]++; // 
     /* Debug: printf ("c=%d (%x), cnttable[%d]=%d\n", (int)c, (int)c, i, cnttable[i]); */
     return;
 }
--- 7.2_Dynamic_Tools/fuzz/report/search.c
+++ 7.2_Dynamic_Tools/fuzz/report/search.c
@@ -95,12 +95,16 @@ Search ()
 
     fprintf (stderr, "> ");
     fflush (stderr);
-    while (gets (inputline))
-    {
+	while (fgets(inputline, MAXLINESIZE, stdin) != NULL) {
 	len = strlen(inputline);
-	for (i=0; i< len; i++) {
-	    countchar(inputline[i]);
+	if (len > 0 && inputline[len - 1] == '\n') {
+		inputline[len - 1] = '\0'; // truncate the ending '\n'
+		len--;
 	}
+	for (i = 0; i < len; i++) {
+		countchar(inputline[i]);
+	}
+
 
 	numargs = GetArgs (inputline, arglist);
 
